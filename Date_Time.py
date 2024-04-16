2878ce6e861fbfada09cdf13e4a2f9390aab970b
Pavlo <74357227+PashaTRN@users.noreply.github.com>
2024-04-16 20:32:46 +0200

    Create Date_Time.py

---
+++ Date_Time.py
@@ -0,0 +1,17 @@
+from datetime import date
+def get_date_range(start, end):
+    lst = []
+    for i in range(start.toordinal(), end.toordinal() + 1):
+        lst.append(date.fromordinal(i))
+    return lst
+
+date1 = date(2021, 10, 1)
+date2 = date(2021, 10, 5)
+
+print(*get_date_range(date1, date2), sep='\n')
+
+////Реализуйте функцию get_date_range(), которая принимает два аргумента в следующем порядке:
+
+start — начальная дата, тип date
+end — конечная дата, тип date
+Функция get_date_range() должна возвращать список, состоящий из дат (тип date) от start до end включительно. Если start > end, функция должна вернуть пустой список
